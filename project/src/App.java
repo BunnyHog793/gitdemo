724ad205b0998d2ecdc2a5a964a38847f990aec9
Christopher Zuehlke <czueh332@students.bju.edu>
2021-03-29 15:46:15 -0400

    duhh

--- project/src/App.java
+++ project/src/App.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 
 public class App {
+    //fguvfghjvghj
     public static void main(String[] args) {
         Application.launch(GUIApplication.class, args);
     }
